bcb885a001d7cbc9a6c8a19d18e57cba4820acfa
James Pegg <jamescpegg@gmail.com>
2013-11-22 22:05:30 +0000

    Collection now uses ArrayIterator
    Now using built-in SPL ArrayIterator as the base for the Collection.

--- src/Common/Collection.php
+++ src/Common/Collection.php
@@ -3,67 +3,26 @@
 namespace Common;
 
 class Collection
-	implements \ArrayAccess, \Iterator
+	extends \ArrayIterator
 {
-	private $collection;
 
-	private $position = 0;
-
-	public function __construct(array $collection = []) {
-		$this->collection = $collection;
-	}
-
-	// Iterator
-
-	public function current() {
-		return $this->collection[$this->position];
-	}
-
-	public function key() {
-		return $this->position;
-	}
-
-	public function next() {
-		++$this->position;
-	}
-
-	public function rewind() {
-		$this->position = 0;
-	}
-
-	public function valid() {
-		return isset($this->collection[$this->position]);
-	}
-
-	// ArrayAccess
-	public function offsetExists($offset)
-	{
-		return isset($this->collection[$offset]);
-	}
-
-	public function offsetGet($offset)
-	{
-		return isset($this->collection[$offset]) ? $this->collection[$offset] : null;
-	}
-
-	public function offsetSet($offset, $value)
-	{
-        if (is_null($offset)) {
-            $this->collection[] = $value;
-        } else {
-            $this->collection[$offset] = $value;
-        }		
-	}
-
-	public function offsetUnset($offset) {
-		unset($this->collection[$offset]);
-	}
-
-	// Array Modifiers
-	
+	/**
+	 * Returns the first item
+	 * @return mixed
+	 */
 	public function first()
 	{
 		$this->rewind();
+		return $this->current();
+	}
+
+	/**
+	 * Returns the last item
+	 * @return mixed
+	 */
+	public function last()
+	{
+		$this->seek($this->count() - 1);
 		return $this->current();
 	}
 
