00d6a87f510ba3fee8140af8241320722df66d89
James Pegg <jamescpegg@gmail.com>
2013-12-15 11:03:26 +0000

    Easy 301 redirects #1

---
+++ src/WebServices/Redirect.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace WebServices;
+
+/**
+ * The redirect class handles permanent (301) and temporary (302)
+ * redirects. It can also take an array of redirects and redirect
+ * a request.
+ *
+ * @author James Pegg <jamescpegg@gmail.com>
+ */
+class Redirect
+{
+	/**
+	 * Permanent redirect
+	 * @param  string $url
+	 */
+	public static function permanent($url)
+	{
+		http_response_code(301);
+		self::location($url);
+	}
+
+	/**
+	 * Temporary redirect
+	 * @param  string $url
+	 */
+	public static function temporary($url)
+	{
+		http_response_code(302);
+		self::location($url);
+	}
+
+	/**
+	 * Takes an array of redirects and processes them.
+	 * @param  array  $redirects
+	 * @param  string $request
+	 * @throws Exception If unsupported redirect type is given
+	 */
+	public static function fromArray(array $redirects, $request)
+	{
+		if (isset($redirects[$request])) {
+			$redirect = $redirects[$request];
+
+			if (is_string($redirect)) {
+				self::temporary($redirect);
+			} elseif (is_array($redirect)) {
+				$url = key($redirect);
+
+				switch ($redirect[$url]) {
+					case '302':
+						self::temporary($url);
+						break;
+
+					case '301':
+						self::permanent($url);
+						break;
+
+					default:
+						throw new \Exception("Redirect type {$redirect[$url]} not currently supported.", 1);
+						break;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Set header location
+	 * @param  string $url
+	 * @throws InvalidArgumentException If arguments are invalid
+	 */
+	private static function location($url)
+	{
+		if (is_string($url)) {
+			header("Location: {$url}");
+			die();			
+		} else {
+			throw new \InvalidArgumentException("Expecting a String, '{$url}' given.");
+		}
+
+	}
+}
